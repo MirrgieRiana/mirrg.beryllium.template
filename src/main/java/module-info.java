f02ea16c7cf1f45ca0c0faab19c0f8536e4fcd81
Mirrgie Riana <tacticsrealize2@gmail.com>
2018-08-11 17:19:13 +0900

    comment

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module mirrg.beryllium.template
 {
-	exports mirrg.beryllium.template;
-	exports mirrg.beryllium.template.core;
+	exports mirrg.beryllium.template; // 主に使ってほしいもの
+	exports mirrg.beryllium.template.core; // 普段は使わないがオプションで触らせたいもの
+	// exports mirrg.beryllium.template.impl; // 隠蔽したいパッケージ
 
 	requires static org.junit.jupiter.api;
 }
